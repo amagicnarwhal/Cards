d22dbdaa71b21dbf79cfeb59d031a97ac350e54a
Odie <oha0405@gmail.com>
2021-04-06 19:43:18 +0100

    dotnet init

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Uni_Assignment2_OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git!");
         }
     }
 }
